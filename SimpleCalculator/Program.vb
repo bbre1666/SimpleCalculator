c9682fe0c7908810e2ba14b8a7b674f2f4d1731e
bbre1666 <badenbrenner@isu.edu>
2023-01-20 08:52:58 -0700

    working calculator

--- SimpleCalculator/Program.vb
+++ SimpleCalculator/Program.vb
@@ -1,28 +1,52 @@
 Imports System
 Imports System.Diagnostics.CodeAnalysis
+Imports System.Net.Security
 
 Module Program
     Sub Main(args As String())
         'declare varibles 
         Dim firstnumber As Integer
-        Dim secondnumber As Decimal
-        Dim result As Decimal
-        Dim testresult As Boolean = True
+        Dim secondnumner As Integer
+        Dim result As String
         Dim userimput As String
 
-        Console.WriteLine("Hello Ready for some math")
+        Console.WriteLine("Hello Ready for some adtion")
 
         'assign varables 
         Console.WriteLine("What is the first number")
         firstnumber = Console.ReadLine()
         Console.WriteLine("What is the second number")
-        secondnumber = Console.ReadLine()
-        Console.WriteLine("what operation would you like preformed")
+        secondnumner = Console.ReadLine()
+        Console.WriteLine("what operation would you like preformed (+ *)")
         userimput = Console.ReadLine()
 
-        'preform operation
-        Console.WriteLine(("expretion") & " " & firstnumber & userimput & secondnumber & ("="))
-        'result += firstnumber userimput secondnumber
+        'preform operation +
+        If userimput = "+" Then
+            Console.WriteLine(("expretion") & " " & firstnumber & "+" & secondnumner & ("=") & result)
+            result = firstnumber + secondnumner
+            Console.WriteLine(result)
+        End If
+
+        'preform operation -
+        If userimput = "-" Then
+            Console.WriteLine(("expretion") & " " & firstnumber & "-" & secondnumner & ("=") & result)
+            result = firstnumber - secondnumner
+            Console.WriteLine(result)
+        End If
+
+        'preform operation *
+        If userimput = "*" Then
+            Console.WriteLine(("expretion") & " " & firstnumber & "*" & secondnumner & ("=") & result)
+            result = firstnumber * secondnumner
+            Console.WriteLine(result)
+        End If
+
+        'preform operation *
+        If userimput = "/" Then
+            Console.WriteLine(("expretion") & " " & firstnumber & "/" & secondnumner & ("=") & result)
+            result = firstnumber / secondnumner
+            Console.WriteLine(result)
+        End If
 
     End Sub
 End Module
